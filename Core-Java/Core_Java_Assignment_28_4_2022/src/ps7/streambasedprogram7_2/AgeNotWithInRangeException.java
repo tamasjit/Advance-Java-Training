4b7b429a8c2086c49f979f921b516bc6fb5285b1
Tamasjit Ghoshal <49725194+tamasjit@users.noreply.github.com>
2022-04-28 17:44:21 +0530

    update

--- Core-Java/Core_Java_Assignment_28_4_2022/src/ps7/streambasedprogram7_2/AgeNotWithInRangeException.java
+++
@@ -1,9 +0,0 @@
-package ps7.streambasedprogram7_2;
-
-class AgeNotWithInRangeException extends Exception
-{
-     public String toString()
-     {
-          return ("Age is not between 15 and 21. please ReEnter the Age");
-     }
-}
--- Core-Java/Core_Java_Assignment_28_4_2022/src/ps7/streambasedprogram7_2/NameNotValidException.java
+++
@@ -1,9 +0,0 @@
-package ps7.streambasedprogram7_2;
-
-public class NameNotValidException extends Exception
-{
-    public String validname()
-    {
-         return ("Name is not Valid..Please ReEnter the Name");
-    }
-}
