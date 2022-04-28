4a890641ed097c1e565b1eb0631b8575c35af3d5
Tamasjit Ghoshal <49725194+tamasjit@users.noreply.github.com>
2022-04-28 16:23:07 +0530

    update

---
+++ Core-Java/Core_Java_Assignment_Complete/src/ps6/studentarraylist6_1/StudentNameArrayList.java
@@ -0,0 +1,35 @@
+package ps6.studentarraylist6_1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class StudentNameArrayList {
+   
+	public static void main(String[] args) {
+		ArrayList<String> a1=new ArrayList<String>();
+		
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of students :");
+		n=sc.nextInt();
+		System.out.println("Enter the student names :");
+		
+		for (int i=0; i<n; i++) {
+			
+			a1.add(sc.next());
+			
+		}
+		System.out.println("student list :");
+		
+		for (String string : a1) {
+			
+			System.out.println("Enter the name of serachable student :");
+			String st=sc.next();
+			int position=Collections.binarySearch(a1,st);
+			System.out.println("position of"+st+"is :"+position);
+			
+		}
+	}
+
+}
